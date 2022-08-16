a7e66a3c563fb482054fc32e610a8814df2efe03
JFlo21 <90293985+JFlo21@users.noreply.github.com>
2022-08-16 12:25:40 -0500

    Initialize mensjacketspage

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,89 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {OwlModule} from 'ngx-owl-carousel';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CustomerloginComponent } from './components/customerlogin/customerlogin.component';
+import { CustomerregisterComponent } from './components/customerregister/customerregister.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { BusinessregisterComponent } from './components/businessregister/businessregister.component';
+import { BusinessloginComponent } from './components/businesslogin/businesslogin.component';
+import { CartComponent } from './components/cart/cart.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
+import { ViewoneitemComponent } from './components/viewoneitem/viewoneitem.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { FooterComponent } from './footer/footer.component';
+import { AllmensshoesComponent } from './allmensshoes/allmensshoes.component';
+import { Mensshoes1Component } from './allmensshoes/mensshoes1/mensshoes1.component';
+import { Mensshoes2Component } from './allmensshoes/mensshoes2/mensshoes2.component';
+import { Mensshoes3Component } from './allmensshoes/mensshoes3/mensshoes3.component';
+import { AllmenshatsComponent } from './allmenshats/allmenshats.component';
+import { AllmensjacketsComponent } from './allmensjackets/allmensjackets.component';
+import { Mensjackets1Component } from './allmensjackets/mensjackets1/mensjackets1.component';
+import { Mensjackets2Component } from './allmensjackets/mensjackets2/mensjackets2.component';
+import { Mensjackets3Component } from './allmensjackets/mensjackets3/mensjackets3.component';
+import { Mensjackets4Component } from './allmensjackets/mensjackets4/mensjackets4.component';
+import { Mensjackets5Component } from './allmensjackets/mensjackets5/mensjackets5.component';
+import { Mensjackets6Component } from './allmensjackets/mensjackets6/mensjackets6.component';
+import { Mensjackets7Component } from './allmensjackets/mensjackets7/mensjackets7.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CustomerloginComponent,
+    CustomerregisterComponent,
+    HomeComponent,
+    ShopComponent,
+    BusinessregisterComponent,
+    BusinessloginComponent,
+    CartComponent,
+    InventoryComponent,
+    EditComponent,
+    CreateComponent,
+    ViewoneitemComponent,
+    PaymentComponent,
+    FooterComponent,
+    AllmensshoesComponent,
+    Mensshoes1Component,
+    Mensshoes2Component,
+    Mensshoes3Component,
+    AllmenshatsComponent,
+    AllmensjacketsComponent,
+    Mensjackets1Component,
+    Mensjackets2Component,
+    Mensjackets3Component,
+    Mensjackets4Component,
+    Mensjackets5Component,
+    Mensjackets6Component,
+    Mensjackets7Component
+   
+  
+
+   
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    OwlModule
+  
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
